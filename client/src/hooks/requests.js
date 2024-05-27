419026c809dcc1cba7a33202e708dd7268ff0368
VENKATA NARAYANAN <prakash1107@gmail.com>
2024-05-27 18:17:39 -0400

    nasa-project initial-commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,26 @@
+const API_BASE_URL = "http://localhost:8000";
+
+async function httpGetPlanets() {
+  // TODO: Once API is ready.
+  // Load planets and return as JSON.
+  const res = await fetch(`${API_BASE_URL}/planets`);
+  console.log(res);
+  return await res.json();
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
